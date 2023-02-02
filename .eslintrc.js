216a6b97001fafdeb3c9f810f953e13618644855
Ryan <656040874@qq.com>
2023-02-02 15:37:20 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
     'object-curly-spacing': ['error', 'always'],
     'switch-colon-spacing': 'error',
     'space-before-blocks': 'error',
-    // 'space-before-function-paren': 'error',
+    // 'space-before-function-paren': 'error'
     'spaced-comment': 'error',
     'no-trailing-spaces': 'error',
     'no-undef': 'off',
